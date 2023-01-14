80b1e4da06ba379fe0b27560ce8249a1bd15a9e2
wuka <1649422875@qq.com>
2023-01-14 13:34:31 +0800

    添加项目文件。

---
+++ 01 C++书写HelloWorld/01 C++书写HelloWorld/02 注释.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+/*
+	多行注释
+	main 是一个程序的入口，每个程序都必须有这么一个函数，有且仅有一个。
+*/
+int main() {
+
+	// 单行注释
+	cout << "Hello world" << endl;
+
+	system("pause");
+
+	return 0;
+}
